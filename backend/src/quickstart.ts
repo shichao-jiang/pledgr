0b582410f257a623c05c6fb54feaef94d8c7dec4
Shichao <sjiang@epicblockchain.io>
2025-05-15 23:34:32 -0400

    get object info

--- backend/src/quickstart.ts
+++ backend/src/quickstart.ts
@@ -7,22 +7,28 @@ async function check_events() {
     const config = new AptosConfig({ network: Network.DEVNET }); // Specify your own network if needed
     const aptos = new Aptos(config);
 
+    const accountOwnedObjects = await aptos.getAccountOwnedObjects({
+        accountAddress: "0x1", // replace with a real account address
+    });
+
+    console.log(accountOwnedObjects);
     
     const fungibleAssetActivities = await aptos.getFungibleAssetMetadataByAssetType({assetType: "0x1::aptos_coin::AptosCoin"});
-    
+    console.log(fungibleAssetActivities);
+
     const sender = Account.generate();
     const transaction = await aptos.transaction.build.simple({
         sender: sender.accountAddress,
         data: {
             function: "0x3ca8983ddcb73a19cedee0ea857b5113b6a151fa13e343b49178ec7d48d8c867::campaign_manager::create_campaign",
             functionArguments: [
-                Object(fungibleAssetActivities),
+                Object(fungibleAssetActivities).creator_address,
                 10,
                 "0x3ca8983ddcb73a19cedee0ea857b5113b6a151fa13e343b49178ec7d48d8c867",
                 "test",
                 "test",
                 ["test"],
-            ]
+            ],
         }
     });
 
